73957045fca04d08d5d1c3a3edbd9cd85d0f4861
Manish Kushwaha <tzmanish97@gmail.com>
2025-10-28 19:36:21 +0530

    scrapped resource

---
+++ scripts/preprocess_biomes_data.py
@@ -0,0 +1,155 @@
+import json
+from dataclasses import dataclass, field, asdict
+from typing import Dict, List, Optional
+
+@dataclass
+class Scroll:
+    power: int = 0
+    dual: int = 0
+    fragment: int = 0
+
+@dataclass
+class Elite:
+    obelisk: float = 0.0
+    wandering: float = 0.0
+
+@dataclass
+class Biome:
+    name: str
+    wiki: str
+    image: str
+    dlc: str
+    type: str
+    level: float
+    is_spoiler: bool
+    entrances: Dict[int, List[str]] = field(default_factory=dict)
+    exits: Dict[int, List[str]] = field(default_factory=dict)
+    scrolls: Dict[int, Scroll] = field(default_factory=dict)
+    elites: Elite = field(default_factory=Elite)
+    doors: Dict[int, List[str]] = field(default_factory=dict)
+    cursed_chests: float = 0.0
+    timed_door: Optional[int] = None
+
+@dataclass
+class Biomes:
+    wiki_base: str
+    biomes: List[Biome] = field(default_factory=list)
+
+scroll_adapter = lambda scrolls, fragments: Scroll(
+    power = next((item["amount"] for item in scrolls if item["type"] == "Scrolls of Power"), 0),
+    dual = next((item["amount"] for item in scrolls if item["type"] == "Dual Scrolls"), 0),
+    fragment = fragments
+)
+
+with open("scripts/_biomes.json", "r") as f:
+    biomes_src = json.load(f)
+
+with open("scripts/_hierarchy.json", "r") as f:
+    biomes_hierarchy = json.load(f)
+
+castle_generic_name = "Dracula's Castle"
+castle_early_name = "Dracula's Castle (early)"
+castle_late_name = "Dracula's Castle (late)"
+biomes = Biomes(wiki_base = "https://deadcells.wiki.gg")
+for biome_src in biomes_src:
+    if biome_src.get('name') == castle_generic_name:
+        hierarchy_early = biomes_hierarchy.get(castle_early_name)
+        hierarchy_late = biomes_hierarchy.get(castle_late_name)
+        depth3 = Biome(
+            name = castle_early_name,
+            wiki = biome_src.get('wiki'),
+            image = biome_src.get('image'),
+            dlc = biome_src.get('dlc'),
+            type = hierarchy_early.get('type'),
+            level = hierarchy_early.get('level'),
+            is_spoiler = hierarchy_early.get('spoiler')
+        )
+        depth6 = Biome(
+            name = castle_late_name,
+            wiki = biome_src.get('wiki'),
+            image = biome_src.get('image'),
+            dlc = biome_src.get('dlc'),
+            type = hierarchy_late.get('type'),
+            level = hierarchy_late.get('level'),
+            is_spoiler = hierarchy_late.get('spoiler')
+        )
+
+        for bc in range(5):
+            entrances = biome_src.get('entrances').get(str(bc), [])
+            depth3.entrances[bc] = entrances[:3]
+            depth6.entrances[bc] = entrances[3:]
+
+
+            exits = biome_src.get('exits').get(str(bc), [])
+            depth3.exits[bc] = exits[:2]
+            depth6.exits[bc] = exits[2:]
+
+            scrolls = biome_src.get('scrolls').get(str(bc), [])
+            fragments = biome_src.get('scroll_fragments').get(str(bc))
+            depth3.scrolls[bc] = scroll_adapter(scrolls[:2], int(fragments.split(',')[0].split()[0]) if fragments else 0)
+            depth6.scrolls[bc] = scroll_adapter(scrolls[3:5], int(fragments.split(',')[1].split()[0]) if fragments else 0)
+
+            doors = biome_src.get('doors').get(str(bc), [])
+            depth3.doors[bc] = doors
+            depth6.doors[bc] = doors
+        
+        depth6.scrolls[1] = depth3.scrolls[1]
+
+        depth3.elites = depth6.elites = Elite(
+            obelisk = biome_src.get('elite').get('obelisk', 0), 
+            wandering = biome_src.get('elite').get('wandering', 0)
+        )
+
+        depth3.cursed_chests = depth6.cursed_chests = biome_src.get('cursed_chests')
+        depth3.timed_door = depth6.timed_door = biome_src.get('timed_door')
+
+        biomes.biomes.append(depth3)
+        biomes.biomes.append(depth6)
+    
+    else:
+        name = biome_src.get('name')
+        hierarchy = biomes_hierarchy.get(name)
+        biome = Biome(
+            name = name,
+            wiki = biome_src.get('wiki'),
+            image = biome_src.get('image'),
+            dlc = biome_src.get('dlc'),
+            type = hierarchy.get('type'),
+            level = hierarchy.get('level'),
+            is_spoiler = hierarchy.get('spoiler')
+        )
+
+        for bc in range(5):
+            biome.entrances[bc] = biome_src.get('entrances').get(str(bc), [])
+            biome.exits[bc] = biome_src.get('exits').get(str(bc), [])
+
+            scrolls = biome_src.get('scrolls').get(str(bc), [])
+            fragments = biome_src.get('scroll_fragments').get(str(bc), 0)
+            biome.scrolls[bc] = scroll_adapter(scrolls, int(fragments))
+
+            biome.doors[bc] = biome_src.get('doors').get(str(bc), [])
+        
+        biome.elites = Elite(
+            obelisk = biome_src.get('elite').get('obelisk', 0), 
+            wandering = biome_src.get('elite').get('wandering', 0)
+        )
+
+        biome.cursed_chests = biome_src.get('cursed_chests', 0)
+        biome.timed_door = biome_src.get('timed_door')
+
+        biomes.biomes.append(biome)
+
+for castle_name in [castle_early_name, castle_late_name]:
+    castle = next(biome for biome in biomes.biomes if biome.name == castle_name)
+    for bc in range(5):
+        for entrance_name in castle.entrances[bc]:
+            entrance = next(biome for biome in biomes.biomes if biome.name == entrance_name)
+            for key, values in entrance.exits.items():
+                entrance.exits[key] = [castle_name if v == castle_generic_name else v for v in values]
+        for exit_name in castle.exits[bc]:
+            exit = next(biome for biome in biomes.biomes if biome.name == exit_name)
+            for key, values in exit.entrances.items():
+                exit.entrances[key] = [castle_name if v == castle_generic_name else v for v in values]
+
+with open('public/biomes.json', 'x') as f:
+    json.dump(asdict(biomes), f)
---
+++ scripts/scrap_biomes_data.py
@@ -0,0 +1,128 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urlsplit, urlunsplit
+import re
+import json
+from queue import Queue
+from threading import Lock
+import requests
+import time
+from pathlib import Path
+
+class UniqueQueue:
+    def __init__(self):
+        self.q = Queue()
+        self.seen = set()
+        self.lock = Lock()  # ensures atomicity for set + queue ops
+
+    def put(self, item):
+        with self.lock:
+            if item.get('link') not in self.seen:
+                self.seen.add(item.get('link'))
+                self.q.put(item)
+
+    def get(self):
+        return self.q.get()
+
+    def empty(self):
+        return self.q.empty()
+
+base_url = "https://deadcells.wiki.gg"
+queue = UniqueQueue()
+queue.put({ 
+    "name": "Prisoners' Quarters",
+    "link": "/wiki/Prisoners%27_Quarters" 
+    })
+biomes = []
+blacklist = ["Giant"]
+
+while not queue.empty():
+    wait_required = True
+    q_data = queue.get()
+    url = q_data.get('link')
+    name = q_data.get('name')
+    tag = q_data.get('tag')
+    try:
+        path = Path(f"scripts/_scrap/{name}.html")
+        if path.exists():
+            html = path.read_text(encoding="utf-8")
+            wait_required = False
+        else:
+            r = requests.get(f'{base_url}{url}')
+            r.raise_for_status()
+            html = r.text
+            path.write_text(html, encoding="utf-8")
+
+        soup = BeautifulSoup(html, "html.parser")
+        a = soup.select_one("aside.portable-infobox")
+        if a:
+            biome = { 
+                "name": a.find('h2').get_text(),
+                "wiki": url,
+                "image": urlunsplit((*urlsplit([u.split()[0] for u in a.img['srcset'].split(',') if '2x' in u][0])[:3], '', '')),
+                "entrances": {
+                    int(re.search(r'\d+', div['data-source']).group()): [entrance.get_text(strip=True) for entrance in div.find_all('a') if entrance.get_text(strip=True) not in blacklist]
+                    for div in a.find_all('div', attrs={'data-source': re.compile(r'^entrance_\d+$')})
+                },
+                "exits": {
+                    int(re.search(r'\d+', div['data-source']).group()): [entrance.get_text(strip=True) for entrance in div.find_all('a') if entrance.get_text(strip=True) not in blacklist]
+                    for div in a.find_all('div', attrs={'data-source': re.compile(r'^exit_\d+$')})
+                },
+                "scrolls": {
+                    int(re.search(r'\d+', div['data-source']).group()): [{"amount": int(x.strip().split(maxsplit=1)[0]), "type": x.strip().split(maxsplit=1)[1]} for x in ' '.join(div.find('div').get_text().split()).split(',')]
+                    for div in a.find_all('div', attrs={'data-source': re.compile(r'^scrolls_\d+$')})
+                },
+                "elite": {},
+                "doors": {
+                    int(re.search(r'\d+', div['data-source']).group()): [x.strip() for x in ' '.join(div.get_text().split()).split(',')]
+                    for div in a.find_all('div', attrs={'data-source': re.compile(r'^BSC_door_\d+$')})
+                },
+                "scroll_fragments": {
+                    int(re.search(r'\d+', div['data-source']).group()): div.find('div').get_text()
+                    for div in a.find_all('div', attrs={'data-source': re.compile(r'^scroll_frags_\d+$')})
+                }
+            }
+
+            if tag:
+                biome['dlc'] = tag
+
+            timed_door = a.find('div', attrs = {'data-source': 'timed_door'})
+            if(timed_door):
+                time_required = timed_door.find('div').contents[0].split(' ', 1)[0]
+                parts = list(map(int, time_required.split(':')))
+                if len(parts) < 2:
+                    parts.append(0)
+                biome["timed_door"] = parts[0]*60 + parts[1]
+
+            cursed_chests = a.find('div', attrs = {'data-source': 'cursed_chests_0'})
+            if(cursed_chests):
+                biome["cursed_chests"] = float(cursed_chests.find('div').contents[0].strip('%'))/100
+
+            wandering_elites = a.find('div', attrs = {'data-source': 'wandering_elite'})
+            if(wandering_elites):
+                biome["elite"]["wandering"] = float(wandering_elites.find('div').contents[0].strip('%'))/100
+                
+            elite_room = a.find('div', attrs = {'data-source': 'elite_room'})
+            if(elite_room):
+                biome["elite"]["obelisk"] = float(elite_room.find('div').contents[0].strip('%'))/100
+            
+            biomes.append(biome)
+
+            for exit in (a.find('div', attrs = {'data-source': 'exit_4'}).find_all('a') if a.find('div', attrs={'data-source': 'exit_4'}) else []):
+                if exit.get_text(strip=True) not in blacklist:
+                    queue.put({
+                        "name": exit.get_text(strip=True),
+                        "link": exit['href'],
+                        "tag": exit.find_next_sibling().get_text(strip=True) if exit.find_next_sibling() and exit.find_next_sibling().name == 'sup' else None
+                    })
+
+            print(f"{name}: DONE {json.dumps(biome, indent=2)}\n\n")
+        else:
+            print(f"{name}: no infobox found\n\n")
+    except Exception as e:
+        print(f"{name}: ERROR {e}\n\n")
+    finally:
+        if wait_required and not queue.empty():
+            time.sleep(10)
+if biomes:
+    with open('scripts/_biomes.json', 'x') as fp:
+        json.dump(biomes, fp)
