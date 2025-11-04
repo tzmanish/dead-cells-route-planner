b401b4182d5d91af79e753baef2bbfd6735dc41b
Manish Kushwaha <tzmanish97@gmail.com>
2025-11-04 23:41:58 +0530

    chore: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/dead-cells-route-planner/',
   plugins: [
     tailwindcss(),
   ],
   optimizeDeps: {
     exclude: ['leader-line', 'anim-event']
-  }
+  },
 })
