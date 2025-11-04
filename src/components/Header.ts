2f2a2848876209d7139805cae8e9c6dd9629cc90
Manish Kushwaha <tzmanish97@gmail.com>
2025-11-04 23:30:05 +0530

    cicd

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -2,9 +2,9 @@ export function Header(): HTMLElement {
     const header = document.createElement('header');
     header.className = 'bg-dc-header shadow-dc h-[60px]';
     header.innerHTML = `
-      <div class="mx-auto h-full px-6">
-        <div class="flex justify-between items-center h-full">
-          <div class="flex items-center gap-0 font-title font-bold text-5xl">
+      <div class="mx-auto h-full px-4">
+        <div class="flex justify-between items-end h-full">
+          <div class="flex items-baseline gap-0 font-title font-bold text-5xl">
             Route
             <img src="/logo.svg" alt="Dead Cells Route Planner Logo" class="h-[32px]"/>
             Planner
